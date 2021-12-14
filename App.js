12933059b3b88f00153acf7504f49ce4ba9cab2b
Faiaz Umaer <faiazumaer@gmail.com>
2021-12-14 13:48:48 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 
 import AppStack from "./src/navigation/AppStack";
 
 export default function App() {
-  return AppStack();
+  <NavigationContainer>
+    <AppStack />
+  </NavigationContainer>
 }
