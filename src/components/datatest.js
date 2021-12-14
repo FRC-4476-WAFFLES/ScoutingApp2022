ae0e73dd41e9b66eac0a41aea40ea179e4f57291
Habibirb <dodoyoung2017@gmail.com>
2021-12-14 14:37:29 -0500

    formatting

--- src/components/datatest.js
+++ src/components/datatest.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import { View, Text } from 'react-native';
+import { View, Text } from "react-native";
 import styles from "../styles/styles";
 
 const datatest = ({ navigation, route }) => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>hello</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>hello</Text>
+    </View>
+  );
 };
 
 export default datatest;
