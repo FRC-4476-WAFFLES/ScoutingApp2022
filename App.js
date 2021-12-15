1022485d37a4dbe3decec297ffb7a4855ae5e139
Faiaz Umaer <faiazumaer@gmail.com>
2021-12-14 19:03:42 -0500

    Update json paths
    App json changed for assets now in src folder, reverted App.js to working version

--- App.js
+++ App.js
@@ -4,9 +4,5 @@ import loadFonts from "./src/styles/typography";
 import AppStack from "./src/navigation/AppStack";
 
 export default function App() {
-  if (loadFonts()) {
-    return AppStack();
-  }
-  
-  return <AppLoading />
+  return AppStack();
 }
