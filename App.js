2bbf6f8ef635f7e4ecec19976984ef7c0e117432
Habibirb <dodoyoung2017@gmail.com>
2021-12-14 19:10:35 -0500

    Fixed font removed

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import { AppLoading } from 'expo';
-
-import loadFonts from "./src/styles/typography";
 import AppStack from "./src/navigation/AppStack";
 
 export default function App() {
--- src/styles/typography.js
+++
@@ -1,7 +0,0 @@
-import { useFonts } from "@use-expo/font";
-
-export default function loadFonts() {
-    return useFonts({
-        "RobotoSlab Regular": require('../assets/fonts/RobotoSlab-Regular.ttf')
-    });
-}
