d130a2243a19371280d6e648640071ed2db41068
Habibirb <dodoyoung2017@gmail.com>
2021-12-15 19:26:52 -0500

    Started 2nd page

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar, Dimensions } from "react-native";
-import styles from "../styles/styles";
-import dataStyle from '../styles/dataStyle';
+import React from "react";
+import { View, Text } from "react-native";
+import dataStyle from "../styles/dataStyle";
 
 const PageTitle = ({ text }) => {
-    return (
-        <View style={dataStyle.container}>
-            <Text style={dataStyle.title}>{ text }</Text>
-            <View style={dataStyle.bar}></View>
-        </View>
-    )
-}
+  return (
+    <View style={dataStyle.container}>
+      <Text style={dataStyle.title}>{text}</Text>
+      <View style={dataStyle.bar}></View>
+    </View>
+  );
+};
 
 export default PageTitle;
--- src/components/datatest.js
+++ src/components/datatest.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, StatusBar, Dimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, TextInput } from "react-native";
 
 import PageTitle from "./PageTitle";
 import dataStyle from "../styles/dataStyle";
 
 const datatest = ({ navigation, route }) => {
+  const [teamNumber, onChangeNumber] = React.useState("null");
+
   return (
     <SafeAreaView style={dataStyle.container}>
-      <PageTitle text='Submission' />
+      <PageTitle text="Submission" />
+      <View style={[dataStyle.container, dataStyle.alignHeaders]}>
+        <Text style={dataStyle.teamHead}>Team #</Text>
+        <TextInput
+          style={dataStyle.input}
+          onChangeText={onChangeNumber}
+          value={teamNumber}
+          placeholder="Team Number"
+          keyboardType="numeric"
+        ></TextInput>
+        <Text style={dataStyle.headings}>Overall Performance</Text>
+        <Text style={dataStyle.headings}>Points Scored</Text>
+      </View>
     </SafeAreaView>
   );
 };
