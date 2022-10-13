7463b818364080ee5fa8e8765aabe52300f33b61
Faiaz Umaer <faiazumaer@gmail.com>
2022-10-13 17:47:58 -0400

    Delete everything

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { registerRootComponent } from 'expo';
-import React from 'react';
-import AppStack from "./navigation/AppStack";
-
-const App = () => {
-  return <AppStack />
-}
-
-registerRootComponent(App);
