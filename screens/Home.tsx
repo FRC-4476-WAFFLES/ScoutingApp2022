46e72735188f527c377393dba53dc7ff0430fdad
Faiaz Umaer <faiazumaer@gmail.com>
2022-10-14 14:58:56 -0400

    Home.tsx and remove styled components

---
+++ screens/Home.tsx
@@ -0,0 +1,17 @@
+import { FunctionComponent } from "react";
+import { 
+    SafeAreaView,
+    Text,
+} from "react-native";
+
+import { colors } from "../components/colors";
+
+const Home: FunctionComponent = () => {
+    return (
+        <SafeAreaView>
+            
+        </SafeAreaView>
+    );
+}
+
+export default Home;
