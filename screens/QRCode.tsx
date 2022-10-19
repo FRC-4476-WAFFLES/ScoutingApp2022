a5585319769a35dc965bce0d7292f8149a8ae35b
Faiaz Umaer <faiazumaer@gmail.com>
2022-10-19 19:48:15 -0400

    Update QRCode.tsx

--- screens/QRCode.tsx
+++ screens/QRCode.tsx
@@ -10,6 +10,7 @@ import {
   Dimensions
 } from "react-native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { captureScreen } from "react-native-view-shot";
 
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
@@ -21,16 +22,88 @@ import StackParamList from "../library/StackParamList";
 
 type QRCodeScreenProps = NativeStackScreenProps<StackParamList, "QRCode">;
 
-const QRCodeScreenProps: React.FunctionComponent<QRCodeScreenProps> = props => {
+const QRCodeScreen: React.FunctionComponent<QRCodeScreenProps> = props => {
+    const { navigation, route } = props;
+    const csvData = route.params.data;
+
+    React.useEffect(() => {
+        const getPermissions = async () => {
+            const permission = await MediaLibrary.requestPermissionsAsync()
+            if (!permission) {
+                // Do something
+            }
+        }
+    
+        getPermissions();
+      })
 
     return (
         <SafeAreaView>
-
-
-
+            <ScrollView>
+                <ScreenTitle title={"QR Code"} />
+                <View style={styles.qrcodeContainer}>
+                    {/* <QRCode value={route.params.data} size={300} /> */}
+                </View>
+                <View style={styles.displayText}>
+                    <Text style={styles.text}>
+                    CSV: {csvData}
+                    </Text>
+                    <Text style={styles.text}>
+                    {getDataFormatted(route.params.data)}
+                    </Text>
+                </View>
+                <TouchableOpacity onPress={() => captureQR()} style={styles.button}>
+                    <Text style={styles.buttonText}>Save Image</Text>
+                </TouchableOpacity>
+            </ScrollView>
         </SafeAreaView>
     );
+
+    async function captureQR() {
+        try {
+          const result = await captureScreen({
+            result: "tmpfile",
+            quality: 1,
+            format: "png",
+          });
+          await MediaLibrary.saveToLibraryAsync(result);
+        } catch (e) {
+          console.log(e);
+        }
+    }
+    
+    function getDataFormatted(data: string) {
+        let arr = CSVtoArray(data);
+    
+        if (arr == null) return;
+        let match = arr[1];
+        return `Match: ${match}`;
+    }
+    
+    // BY ridgerunner OF STACK OVREFLOW
+    // Return array of string values, or NULL if CSV string not well formed.
+    function CSVtoArray(text: string) {
+        var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+        var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+        // Return NULL if input string is not well formed CSV string.
+        if (!re_valid.test(text)) return null;
+        var a = [];                     // Initialize array to receive values.
+        text.replace(re_value, // "Walk" the string using replace with callback.
+            function(m0, m1, m2, m3) {
+                // Remove backslash from \' in single quoted values.
+                if      (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+                // Remove backslash from \" in double quoted values.
+                else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+                else if (m3 !== undefined) a.push(m3);
+                return ''; // Return empty string.
+            });
+        // Handle special case of empty last value.
+        if (/,\s*$/.test(text)) a.push('');
+        return a;
+    };
 }
+
+export default QRCodeScreen;
 
 const styles = StyleSheet.create({
     qrcodeContainer: {
@@ -45,6 +118,10 @@ const styles = StyleSheet.create({
     text: {
       fontSize: 40,
       marginTop: 40
+    },
+
+    displayText: {
+
     },
   
     button: {
